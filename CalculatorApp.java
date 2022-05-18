177bfad2c1a11d922803df35dd69f90bd875e5a6
Raj <rajtrivedi99@gmail.com>
2022-05-18 15:16:47 -0400

    Java Program for calculator

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,9 +1,39 @@
-
+import java.util.*;
 public class CalculatorApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Java Program for Calculator");
+		double a, b, result;
+		char op;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your First number: ");
+		a = scan.nextDouble();
+		System.out.println("Enter your Second number: ");
+		b = scan.nextDouble();
+		System.out.println("Please select any operator from '+', '-', '*' or '/'");
+		Scanner sc= new Scanner(System.in);
+		op= scan.next().charAt(0);
+		switch(op) {
+		case '+':
+			result = a+b;
+			System.out.println("Addition of two number is "+result);
+			break;
+		case '-':
+			result = a-b;
+			System.out.println("Substraction of two number is "+result);
+			break;
+		case '*':
+			result = a+b;
+			System.out.println("Multiplication of two number is "+result);
+			break;
+		case '/':
+			result = a/b;
+			System.out.println("Devision of two number is "+result);
+			break;
+		default:
+			System.out.println("Please enter valid operator from '+', '-', '*' or '/'");
+		}
 	}
 
 }
